13067bddd7b3b7aede2020f851d7d912fcace722
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-02-15 10:04:26 -0500

    add lab_12-1.py lab_12-2.py my_file.txt

---
+++ lab_12-1.py
@@ -0,0 +1,5 @@
+# Author: ATN 2/15/22
+
+ofile = open("my_file.txt", "w")
+ofile.write("ATN 2/15/22\nHello World!\nMaking music.")
+ofile.close()
---
+++ lab_12-2.py
@@ -0,0 +1,3 @@
+ofile = open("my_file.txt", "a")
+ofile.write("\nStaying in contact with my friends.\nGoing on a trip to Norway.")
+ofile.close()
