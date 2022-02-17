8beffa36aa8150c24009d5f92c70d9090a73ab60
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-02-17 09:48:10 -0500

    add lab_12-5.py

---
+++ lab_12-5.py
@@ -0,0 +1,12 @@
+# Author: ATN 2/17/22
+
+row = {
+    'B202-16':'Aiden', 'Ian':'B202-17', 'Colin':'B202-18', 'Timmy':'B202-19', 'Cam':'B202-20', 'B202-21':'none',
+    'B202-22':'none'
+    }
+
+print(row)
+
+row['B202-16'] = 'none'
+
+print(row)
